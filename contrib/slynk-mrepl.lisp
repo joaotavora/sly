c203969ced625b4560ca42158d1cf030ba1bf3e9
João Távora <joaotavora@gmail.com>
2014-09-10 18:34:06 +0100

    Fix history inspection in sly-mrepl
    * contrib/sly-mrepl.el (sly-mrepl-part): New "describe" and "inspect" actions.
    (listener): Set result-counter to -1.
    
    * contrib/slynk-mrepl.lisp (with-eval-for-repl): New macro.
    (eval-for-mrepl): Use it.
    (inspect-entry): Use it.
    (describe-entry, pprint-entry): New functions.

--- contrib/slynk-mrepl.lisp
+++ contrib/slynk-mrepl.lisp
@@ -249,24 +249,39 @@
   (setq *saved-objects* (multiple-value-list (apply slave-slyfun args)))
   t)
 
+(defmacro with-eval-for-repl ((remote-id) &body body)
+  (let ((mrepl-sym (gensym)))
+    `(let ((,mrepl-sym (find-channel ,remote-id)))
+       (assert ,mrepl-sym)
+       (assert
+        (eq (slynk-backend:thread-id
+             (slynk-backend:current-thread)) 
+            (channel-thread-id ,mrepl-sym))
+        nil
+        "This SLYFUN can only be called from threads belonging to MREPL")
+       (with-listener ,mrepl-sym
+         ,@body))))
+
 (defslyfun eval-for-mrepl (remote-id slave-slyfun &rest args)
   "Call SLAVE-SLYFUN with ARGS in the MREPL of REMOTE-ID.
 Both the target MREPL's thread and environment are considered."
-  (let ((mrepl (find-channel remote-id)))
-    (assert mrepl)
-    (assert
-     (eq (slynk-backend:thread-id
-          (slynk-backend:current-thread)) 
-         (channel-thread-id mrepl))
-     nil
-     "This SLYFUN can only be called from threads belonging to MREPL")
-    (with-listener mrepl
-      (apply slave-slyfun args))))
+  (with-eval-for-repl (remote-id)
+    (apply slave-slyfun args)))
 
 (defslyfun inspect-entry (remote-id entry-idx value-idx)
-  (eval-for-mrepl remote-id
-                  'slynk::inspect-object
-                  (mrepl-get-object-from-history entry-idx value-idx)))
+  (with-eval-for-repl (remote-id)
+    (slynk::inspect-object
+     (mrepl-get-object-from-history entry-idx value-idx))))
+
+(defslyfun describe-entry (remote-id entry-idx value-idx)
+  (with-eval-for-repl (remote-id)
+    (slynk::describe-to-string
+     (mrepl-get-object-from-history entry-idx value-idx))))
+
+(defslyfun pprint-entry (remote-id entry-idx value-idx)
+  (with-eval-for-repl (remote-id)
+    (slynk::slynk-pprint
+     (list (mrepl-get-object-from-history entry-idx value-idx)))))
 
 
 ;;;; Dedicated stream
