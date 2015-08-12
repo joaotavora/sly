ca2c8af4ac93ab150db871d0333a76976ec0eccf
João Távora <joaotavora@gmail.com>
2015-08-12 18:26:31 +0100

    Hook sly-named-readtables to sly-extra-mode-line-constructs

--- contrib/sly-named-readtables.el
+++ contrib/sly-named-readtables.el
@@ -3,9 +3,27 @@
 
 (define-sly-contrib sly-named-readtables
   "Automatically parse in-readtable forms in Lisp buffers"
-  (:slynk-dependencies slynk-named-readtables)
-  (:on-load )
-  (:on-unload ))
+  ;; (:slynk-dependencies slynk-named-readtables)
+  (:on-load (add-hook 'sly-editing-mode-hook 'sly-named-readtables-mode))
+  (:on-unload (remove-hook 'sly-editing-mode-hook 'sly-named-readtables-mode)))
+
+(defun sly-named-readtable--pretty-name (name)
+  (cond ((string-match "^#?:\\(.*\\)$" name)
+         (match-string 1 name))
+        ((string-match "^\"\\(.*\\)\"$" name)
+         (match-string 1 name))
+        (t name)))
+
+(define-minor-mode sly-named-readtables-mode
+  "Use EDITOR-HINTS.NAMED-READTABLES if available."
+  nil nil nil
+  (if sly-named-readtables-mode
+      (add-to-list 'sly-extra-mode-line-constructs
+                   'sly-named-readtables--mode-line-construct
+                   t)
+    (setq sly-extra-mode-line-constructs
+          (delete 'sly-named-readtables--mode-line-construct
+                  sly-extra-mode-line-constructs))))
 
 (defun sly-named-readtables--grok-current-table ()
   (let ((case-fold-search t)
@@ -15,11 +33,16 @@
       (when (re-search-backward regexp nil t)
         (match-string-no-properties 2)))))
 
-;; (add-to-list 'sly-mode-line-format `(:eval (sly-named-readtables--mode-line-format)) 'append)
-
-(defun sly-named-readtables--mode-line-format ()
-   `((,(sly-named-readtables--grok-current-table))))
-
+(defun sly-named-readtables--mode-line-construct ()
+  (let ((readtable-name (sly-named-readtables--grok-current-table)))
+    `(:propertize ,(or (and readtable-name
+                            (sly-named-readtable--pretty-name readtable-name))
+                       "*")
+                  face ,(if readtable-name 'hi-pink 'sly-mode-line)
+                  mouse-face mode-line-highlight
+                  help-echo ,(if readtable-name
+                                 (format "Special NAMED-READTABLE %s" readtable-name)
+                               "Default readtable"))))
 
 (provide 'sly-named-readtables)
 ;;; sly-named-readtables.el ends here
