7836531a32403e821b7735e6c9bc74383b03caa6
João Távora <joaotavora@gmail.com>
2019-12-06 17:35:43 +0100

    Unbreak sly-mrepl-sync broken by 7250f8 (fix for #256)
    * contrib/sly-mrepl.el (sly-mrepl-sync): Unbreak.

--- contrib/sly-mrepl.el
+++ contrib/sly-mrepl.el
@@ -1052,7 +1052,8 @@ prefix argument is given."
     (sly-mrepl--eval-for-repl
      `(slynk-mrepl:sync-package-and-default-directory
        :package-name ,package
-       :directory ,(sly-to-lisp-filename directory))
+       :directory ,(and directory
+                        (sly-to-lisp-filename directory)))
      :insert-p nil
      :before-prompt
      #'(lambda (results)
