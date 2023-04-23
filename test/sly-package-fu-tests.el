6db15f699d5e0e92df90f6cf44bae70f0e4a9f38
Arnold Noronha <arnold@tdrhq.com>
2023-04-23 17:19:17 -0400

    Fix indentation

--- test/sly-package-fu-tests.el
+++ test/sly-package-fu-tests.el
@@ -3,44 +3,44 @@
 (require 'sly-tests "lib/sly-tests")
 
 (def-sly-test sly-package-fu-sly-import (initial-defpackage final-defpackage symbol)
-              "Check if importing `import` on `initial-defpackage` results in `final-defpackage."
-              '(("(defpackage :foo
+  "Check if importing `import` on `initial-defpackage` results in `final-defpackage."
+  '(("(defpackage :foo
   (:use :cl))
 (in-package :foo)"
-                 "(defpackage :foo
+     "(defpackage :foo
   (:use :cl)
   (:import-from #:cl
                 #:find))
 (in-package :foo)"
-                 "cl:find")
-                ("(defpackage :foo
+     "cl:find")
+    ("(defpackage :foo
   (:use :cl)
   (:import-from #:cl
                 #:find))
 (in-package :foo)"
-                 "(defpackage :foo
+     "(defpackage :foo
   (:use :cl)
   (:import-from #:cl
                 #:position
                 #:find))
 (in-package :foo)"
-                 "cl:position")
+     "cl:position")
 
-                ;; TODO: the output here is incorrect, but we're
-                ;; documenting the current behaviour in this test.
-                ("(defpackage :foo
+    ;; TODO: the output here is incorrect, but we're
+    ;; documenting the current behaviour in this test.
+    ("(defpackage :foo
   (:use :cl)
   (:import-from #:bknr.datastore
                 #:find))
 (in-package :foo)"
-                 "(defpackage :foo
+     "(defpackage :foo
   (:use :cl)
   (:import-from #:bknr.datastore
                 #:find)
   (:import-from #:bknr.datastore
                 #:position))
 (in-package :foo)"
-                 "bknr.datastore:position"))
+     "bknr.datastore:position"))
   (let ((file (make-temp-file "sly-package-fu--fixture")))
     (with-temp-buffer
       (find-file file)
