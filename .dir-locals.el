4b2830f046615d1a50dbdaa001bcd493a073e717
João Távora <joaotavora@gmail.com>
2023-04-25 14:16:20 +0100

    Add top-level .dir-locals.el
    This sets the variable elisp-flymake-byte-compile-load-path which
    allows flymake-mode to work properly in the lib/*.el files.
    
    * .dir-locals.el: New file.

---
+++ .dir-locals.el
@@ -0,0 +1,6 @@
+;;; Directory Local Variables            -*- no-byte-compile: t -*-
+;;; For more information see (info "(emacs) Directory Variables")
+
+(("lib"
+  .
+  ((emacs-lisp-mode . ((elisp-flymake-byte-compile-load-path . ("./" "../")))))))
