joaotavora/sly

commit e266e24721db22fddcf8dac53ac3c1618502a47a

Add sly-remove-method

slynk/slynk.lisp
@@ -3198,6 +3198,30 @@ If non-nil, called with two arguments SPEC and TRACED-P." )
   (let ((fname (from-string fname-string)))
     (format nil "~S" (fmakunbound fname))))
 
+(defun read-as-function (name)
+  (eval (from-string (format nil "(function ~A)" name))))
+
+(defslyfun undefine-method (method-name qualifiers specializers)
+  (let* ((generic-function (read-as-function method-name))
+         (qualifiers (mapcar #'from-string qualifiers))
+         (specializers (mapcar #'from-string specializers))
+         (method (find-method generic-function qualifiers specializers)))
+    (format nil "~S"
+            (when method
+              (remove-method generic-function method)))))
+
+(defslyfun method-selectors (method-name)
+  (mapcar
+   (lambda (method)
+     (list (mapcar #'prin1-to-string (slynk-mop:method-qualifiers method))
+           (mapcar (lambda (specializer)
+                     (if (typep specializer 'slynk-mop:eql-specializer)
+                         (format nil "(eql ~A)"
+                                 (sb-mop:eql-specializer-object specializer))
+                         (prin1-to-string (class-name specializer))))
+                   (slynk-mop:method-specializers method))))
+   (slynk-mop:generic-function-methods (read-as-function method-name))))
+
 (defslyfun unintern-symbol (name package)
   (let ((pkg (guess-package package)))
     (cond ((not pkg) (format nil "No such package: ~s" package))

lib/sly-completion.el
@@ -757,6 +757,39 @@ symbol at point, or if QUERY is non-nil."
                  (t (funcall do-it))))
           (t sym-at-point))))
 
+(defun sly--read-method (method-name-prompt selectors-prompt-function)
+  (let* ((method-name (sly-read-symbol-name method-name-prompt t))
+         (format-selectors
+          (lambda (selectors)
+            (let ((qualifiers (car selectors)))
+              (if (null qualifiers)
+                  (format "%s" (cadr selectors))
+                (format "%s %s" (string-join qualifiers " ")
+                        (cadr selectors))))))
+         (selectors-alist
+          (mapcar
+           (lambda (selectors)
+             (cons (funcall format-selectors selectors)
+                   selectors))
+           (sly-eval `(slynk:method-selectors ,method-name))))
+         (selectors-at-point (sly-parse-context method-name)))
+    (when (and (eq :defmethod (car selectors-at-point))
+               (equal method-name (cadr selectors-at-point)))
+      (setq selectors-at-point
+            (string-replace
+             "'" "" (string-join (mapcar #'prin1-to-string
+                                         (cddr selectors-at-point))
+                                 " "))))
+    (unless (cl-member selectors-at-point selectors-alist
+                       :key #'car :test #'equal)
+      (setq selectors-at-point nil))
+    (cons method-name
+          (cdr (assoc (completing-read
+                       (funcall selectors-prompt-function method-name)
+                       (mapcar #'car selectors-alist)
+                       nil t selectors-at-point)
+                      selectors-alist))))))
+
 (provide 'sly-completion)
 ;;; sly-completion.el ends here
 

sly.el
@@ -4386,6 +4386,16 @@ in Lisp when committed with \\[sly-edit-value-commit]."
   (sly-eval-async `(slynk:undefine-function ,symbol-name)
     (lambda (result) (sly-message "%s" result))))
 
+(defun sly-remove-method (name qualifiers specializers)
+  "Remove a method from generic function named NAME.
+The method removed is identified by QUALIFIERS and SPECIALIZERS."
+  (interactive (sly--read-method
+                "[sly] Remove method from which generic function: "
+                (lambda (method-name)
+                  (format "[sly] Remove which method from %s: " method-name))))
+  (sly-eval-async `(slynk:undefine-method ,name ',qualifiers ',specializers)
+    (lambda (result) (sly-message "%s" result))))
+
 (defun sly-unintern-symbol (symbol-name package)
   "Unintern the symbol given with SYMBOL-NAME PACKAGE."
   (interactive (list (sly-read-symbol-name "Unintern symbol: " t)

commit 14bc9d3706c7f40001c29ec1abf7a06ee1958b36

Fix #594, #595: Unbreak for non-SBCL
Broken in e266e24721db22fddcf8dac53ac3c1618502a47a in an undertested PR.

Reported and fix found by @LdBeth

* slynk/slynk.lisp (generic-method-specs): Don't use sb-mop, use
slynk-mop instead.

slynk/slynk.lisp
@@ -3219,7 +3219,7 @@ QUALIFIERS and SPECIALIZERS are lists of strings."
            (mapcar (lambda (specializer)
                      (if (typep specializer 'slynk-mop:eql-specializer)
                          (format nil "(eql ~A)"
-                                 (sb-mop:eql-specializer-object specializer))
+                                 (slynk-mop:eql-specializer-object specializer))
                          (prin1-to-string (class-name specializer))))
                    (slynk-mop:method-specializers method))))
    (slynk-mop:generic-function-methods (read-as-function generic-name))))