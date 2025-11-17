d771417e8eac724f4c0870e645ccc0ffcfddf334
André A. Gomes <andremegafone@gmail.com>
2025-11-17 19:59:23 +0300

    .dir-locals: Add whitespace rules.

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,7 +1,9 @@
 ;;; Directory Local Variables            -*- no-byte-compile: t -*-
 ;;; For more information see (info "(emacs) Directory Variables")
 
-(("lib" .
+((nil . ((require-final-newline . t)
+         (eval . (add-hook 'before-save-hook 'delete-trailing-whitespace nil t))))
+ ("lib" .
   ((emacs-lisp-mode . ((elisp-flymake-byte-compile-load-path . ("./" "../"))))))
  ("contrib" .
   ((emacs-lisp-mode . ((elisp-flymake-byte-compile-load-path . ("./" "../"))))))
